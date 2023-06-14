339355d38901e3540b6a2f67a9572989315df62f
GuoDCZ <richard_zhong@sjtu.edu.cn>
2023-06-15 02:49:45 +0800

    updateMotionMech

--- merge2048.js
+++ merge2048.js
@@ -1,145 +1,205 @@
 G.F.loadMain = function () {
     this.AI = G.F.mainAI;
+
+    var w = window.innerWidth;
+    if (!w) w = 1080;
+    var h = window.innerHeight;
+    if (!h) h = 720;
+
     var arr = [];
-    for (let i = 0; i < 40; i++) arr.push(i);
-    G.setState({ballId:arr})
+    var maxArr = Math.floor(w*h/25000);
+    for (let i = 0; i < maxArr; i++) arr.push(i);
+
+    var colors = [
+        "#E4E6E1", "#E8D3C0", "#C2CEDC", "#D6C38B", 
+        "#D4BAAD", "#B0B1B6", "#849B91", "#D89C7A" 
+    ]
+
+    G.setState({ballId: arr})
     G.makeGob('viewport', G)
-        .setVar({
-            mma: function () {
-                console.log("fuck");
-            }, x: 0, y: 0, w: 500, h: 500 })
-        .setStyle({ backgroundColor: '#000000' })
+        .setVar({x: 0, y: 0, w: w, h: h })
+        .setStyle({ backgroundColor: '#333333' })
         .turnOn();
     G.S.ballId.forEach(id => {
-        var s = Math.ceil(Math.random()*6);
+        var s = Math.ceil((1-id/maxArr)*8);
         var m = Math.pow(2,s);
         var r = Math.sqrt(m)*6;
         G.makeGob(id, G.O.viewport)
             .setVar({
                 x: G.O.viewport.w/2, 
                 y: G.O.viewport.h/2, 
-                r: r,
-                m: m,
+                r: r, m: m,
                 w: 2*r, h: 2*r, 
                 tx: -r, ty: -r,
-                vx: Math.random()*6-3, vy: Math.random()*6-3, 
+                vx: Math.random()*10-5, vy: Math.random()*10-5, 
                 //vx: 0, vy: -2*i,
-                setNextXY: G.F.Ball_setNextXY,
-                checkWallCollision: G.F.Ball_checkWallCollision,
-                fixWallCollision: G.F.Ball_fixWallCollision,
-                checkBallCollision: G.F.Ball_checkBallCollision,
-                getCollisionImpulse: G.F.Ball_getCollisionImpulse,
-                fixBallCollision: G.F.Ball_fixBallCollision
+                setNextXY: function () {
+                    var t = this;
+                    t.setVar({
+                        nx: t.x + t.vx, 
+                        ny: t.y + t.vy
+                    });
+                },
             })
             .addClass('ball')
-            .setStyle({ backgroundColor: '#'+Math.floor(s/7*0xffffff).toString(16).padEnd(6,'0')})
+            .setStyle({ backgroundColor: colors[s-1]})
             .turnOn();
     });
+    G.S.ballId.forEach(id => G.O[id].setNextXY());
+    G.S.ballId.forEach(id => G.O[id].incrementXY());
 }; 
 
-G.F.Ball_setNextXY = function () {
-    var t = this;
-    t.setVar({nx: t.x+t.vx, ny:t.y+t.vy});
+G.F.checkWallIntersection = function (t, wall) {
+    switch (wall) {
+        case "x-": return t.nx < t.r;
+        case "x+": return t.nx > t.P.w - t.r;
+        case "y-": return t.ny < t.r;
+        case "y+": return t.ny > t.P.h - t.r;
+        default: return false;
+    }
 };
 
-G.F.Ball_checkWallCollision = function () {
-    var t = this, p = this.P;
-    if (p.w - t.nx < t.r && t.vx >= 0
-           || t.nx < t.r && t.vx <= 0 ) return 'x';
-    if (p.h - t.ny < t.r && t.vy >= 0
-           || t.ny < t.r && t.vy <= 0 ) return 'y';
+G.F.checkWallApproaching = function (t, wall) {
+    switch (wall) {
+        case "x-": return t.vx < 0;
+        case "x+": return t.vx > 0;
+        case "y-": return t.vy < 0;
+        case "y+": return t.vy > 0;
+        default: return false;
+    }
+}
+
+G.F.fixVelocityByWallCollision = function (t, wall) {
+    switch (wall) {
+        case "x-": case "x+": t.setVar({vx: -t.vx}); break;
+        case "y-": case "y+": t.setVar({vy: -t.vy}); break;
+    }
+};
+
+G.F.fixPositionByWallCollision = function (t, wall) {
+    switch (wall) {
+        case "x-": t.setVar({x: t.r}); break;
+        case "x+": t.setVar({x: t.P.w - t.r}); break;
+        case "y-": t.setVar({y: t.r}); break;
+        case "y+": t.setVar({y: t.P.h - t.r}); break;
+    }
+};
+
+G.F.testWallCollision = function (t, wall) {
+    if (G.F.checkWallIntersection(t, wall)) {
+        if (G.F.checkWallApproaching(t, wall))
+            G.F.fixVelocityByWallCollision(t, wall);
+        else
+            G.F.fixPositionByWallCollision(t, wall);
+        t.setNextXY();
+        t.addClass("highlight");
+        return true;
+    }
     return false;
 };
 
-G.F.Ball_fixWallCollision = function (force) {
-    var t = this;
-    if (force == 'x') t.setVar({vx:-t.vx});
-    if (force == 'y') t.setVar({vy:-t.vy});
+G.F.getBallRelativeDirection = function (a, b) {
+    var x = b.x - a.x,
+        y = b.y - a.y;
+    var d = (x**2+y**2)**0.5;
+    return d ? {sin: y/d, cos: x/d} : null;
 };
 
-G.F.Ball_checkBallCollision = function (b) {
-    var a = this;
-    var nx = b.nx-a.nx,
-        ny = b.ny-a.ny;
-    var x = b.x-a.x,
-        y = b.y-a.y;
-    var vx = b.vx-a.vx,
-        vy = b.vy-a.vy;
-    var toIntersect = (a.r+b.r)**2 > nx**2 + ny**2;
-    var intersectDepth = (a.r+b.r)**2 - x**2 + y**2;
-    var isApproaching = (x*vx+y*vy < 0);
-    return toIntersect && isApproaching;
+G.F.checkBallIntersection = function (a, b) {
+    var dnx = b.nx - a.nx,
+        dny = b.ny - a.ny;
+    return (a.r+b.r)**2 > dnx**2 + dny**2;
 };
 
-G.F.Ball_getCollisionImpulse =  function (b) {
-    var a = this;
-    var x = b.x-a.x,
-        y = b.y-a.y,
-        d = (x**2+y**2)**0.5;
-    var s = y/d,
-        c = x/d;
-    var m = a.m*b.m/(a.m+b.m);
-    var vx = b.vx-a.vx,
-        vy = b.vy-a.vy;
-        v = vy*s+vx*c;
-    return {
-        x: 2*m*v*c,
-        y: 2*m*v*s
+G.F.checkBallApproaching = function (a, b) {
+    var dx = b.x - a.x,
+        dy = b.y - a.y;
+    var dvx = b.vx - a.vx,
+        dvy = b.vy - a.vy;
+    return dx * dvx + dy * dvy < 0;
+};
+
+G.F.getBallCollisionImpulse =  function (a, b) {
+    var d = G.F.getBallRelativeDirection(a, b); // 相对方向
+    if (!d) return {x: 0, y: 0};
+    var m = a.m * b.m / (a.m + b.m);        // 约化质量
+    var vx = b.vx - a.vx,                   // 相对速度
+        vy = b.vy - a.vy;
+    var v = vy * d.sin + vx * d.cos;        // 相对速度在相对方向上分量
+    return {                                // 冲量
+        x: 2 * m * v * d.cos,                
+        y: 2 * m * v * d.sin
     };
 };
 
-G.F.Ball_fixBallCollision = function (I) {
-    var t = this;
-    t.setVar({vx:t.vx+I.x/t.m, vy:t.vy+I.y/t.m});
-
+G.F.fixVelocityByImpulse = function (t, I) {
+    t.setVar({
+        vx: t.vx + I.x / t.m, 
+        vy: t.vy + I.y / t.m
+    });
 }
 
-G.F.mainAI = function () {
-    G.S.ballId.forEach(id => G.O[id].setNextXY());
-    var flush = 0;
-    while (G.F.fixCollision() && flush < 100) {
-        G.S.ballId.forEach(id => G.O[id].setNextXY());
-        flush++;
-    }
-    if (flush == 100) console.log("F!");
-    G.S.ballId.forEach(id => G.O[id].incrementXY());
-    //G.S.ballId.forEach(id => G.O[id].setVar({vy:G.O[id].vy*0.9+1}));
-    G.S.ballId.forEach(id => G.O[id].draw());
-    var E = G.F.getTotalEnergy();
-    console.log(E);
+G.F.fixPositionByBallCollision = function (a, b) {
+    var d = G.F.getBallRelativeDirection(a, b);
+    if (!d) return;
+    console.log("C!");
+    var ms = a.m + b.m;
+    a.setVar({x: a.x-d.cos*a.m/ms, y: a.y-d.sin*a.m/ms});
+    b.setVar({x: b.x+d.cos*b.m/ms, y: b.y+d.sin*b.m/ms});
 };
 
-G.F.getTotalEnergy = function () {
-    var E = 0;
-    G.S.ballId.forEach(id => {
-        var t = G.O[id];
-        E += t.m * (t.vx**2+t.vy**2) / 2;
-    });
-    return E;
-};
-
-G.F.fixCollision = function () {
-    var flag = false;
-    G.S.ballId.forEach(id => {
-        var t = G.O[id];
-        var force;
-        if (force = t.checkWallCollision()) {
-            t.fixWallCollision(force);
-            flag = true;
+G.F.testBallCollision = function (a, b) {
+    //if (a == b) return false;
+    if (G.F.checkBallIntersection(a, b)) {
+        if (G.F.checkBallApproaching(a, b)) {
+            var I = G.F.getBallCollisionImpulse(a, b);
+            G.F.fixVelocityByImpulse(a, I);
+            G.F.fixVelocityByImpulse(b, {x:-I.x,y:-I.y});
+            //return true;
         }
-        G.S.ballId.forEach(id_ => {
-            var b = G.O[id_];
-            if (b == t) return;
-            if (t.checkBallCollision(b)) {
-                console.log("C!")
-                var I = t.getCollisionImpulse(b);
-                t.fixBallCollision(I);
-                b.fixBallCollision({x:-I.x, y:-I.y});
-                flag = true;
-            }
-        })
-    });
-    return flag;
+        else {
+            G.F.fixPositionByBallCollision(a, b);
+        }
+        a.setNextXY();
+        b.setNextXY();
+        a.addClass("highlight");
+        b.addClass("highlight");
+        return true;
+    }
+    return false;
+};
+
+G.F.mainAI = function () {
+    G.S.ballId.forEach(id => G.O[id].removeClass("highlight"));
+    G.S.ballId.forEach(id => G.O[id].setNextXY());
+    G.F.testCollision();
+    G.S.ballId.forEach(id => G.O[id].incrementXY());
+    G.S.ballId.forEach(id => G.O[id].draw());
+};
+
+G.F.testCollision = function (maxLoop = 1000) {
+    var processing = true;
+    var nLoop = 0;
+    while (processing && nLoop < maxLoop) {
+        processing = false;
+        nLoop++;
+        G.S.ballId.forEach(id => {
+            var a = G.O[id];
+            ["x-","x+","y-","y+"].forEach(wall => {
+                if (G.F.testWallCollision(a, wall)) processing = true; 
+            });
+            var pass = true;
+            G.S.ballId.forEach(id_another => {
+                if (pass) {
+                    if (id_another == id) pass = false;
+                    return;
+                }
+                var b = G.O[id_another];
+                if (G.F.testBallCollision(a, b)) processing = true;
+            })
+        });
+    }
+    if (nLoop == maxLoop) console.log("F!");
 }
 
 
